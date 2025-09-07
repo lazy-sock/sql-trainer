98ae8c55c5fc52fa7c7019e8319ff848d176e3e2
lazy-sock <paul.k@unity-mail.de>
2025-09-07 17:57:12 +0200

    add basic sql-queries

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 use rusqlite::{params, Connection, Result};
 use std::io;
 
+#[derive(Debug)]
+struct Person {
+    id: i32,
+    name: String,
+    data: Option<Vec<u8>>,
+}
+
 fn main() -> Result<()> {
     let db = std::env::args().nth(1).expect("No db given");
-    // let parameter = std::env::args().nth(2).expect("No parameter given");
-    println!("pattern: {:?}, path: {:?}", pattern, path);
+    println!("db: {:?}", db);
     
     let mut input = String::new();
-    io::stdin().read_line(&mut input)?;
+//    io::stdin().read_line(&mut input)?;
 
     let connection = Connection::open_in_memory()?;
 
@@ -19,6 +25,26 @@ fn main() -> Result<()> {
         )",
         (), // empty list of parameters.
     )?;
+
+    let binary_data = vec![1u8, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1];
+
+    // Correctly insert the BLOB data using a placeholder
+    connection.execute(
+        "INSERT INTO person (id, name, data) VALUES (?1, ?2, ?3)",
+        params![0, "paul", binary_data],
+    )?;    let mut data = connection.prepare("SELECT * FROM person")?;
+
+    let person_iter = data.query_map([], |row| {
+        Ok(Person {
+            id: row.get(0)?,
+            name: row.get(1)?,
+            data: row.get(2)?,
+        })
+    })?;
+
+    for person in person_iter {
+        println!("{:?}", person?);
+    }
 
     Ok(())
 }
