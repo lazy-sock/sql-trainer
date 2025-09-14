f447e91a6b89996034d48c67cebe5eb5e9de50b7
lazy-sock <paul.k@unity-mail.de>
2025-09-14 19:20:04 +0200

    remove unused code and bad pattern

---
+++ src/lib.rs
@@ -0,0 +1,49 @@
+use std::result::Result;
+
+use rusqlite::Connection;
+use rusqlite::types::ValueRef;
+
+use tabled::{
+    builder::Builder,
+    settings::{Alignment, Style, object::Columns},
+};
+
+pub fn execute_user_select(conn: &Connection, query: &str) -> Result<(), rusqlite::Error> {
+    let mut stmt = conn.prepare(query)?;
+    let col_count = stmt.column_count();
+
+    let col_names: Vec<String> = stmt
+        .column_names()
+        .iter()
+        .map(|s| String::from(*s))
+        .collect::<Vec<_>>();
+
+    let mut rows = stmt.query([])?;
+
+    let mut builder = Builder::default();
+    builder.push_record(col_names.iter().cloned());
+
+    while let Some(row) = rows.next()? {
+        let mut cells = Vec::new();
+        for i in 0..col_count {
+            let value_ref = row.get_ref(i)?;
+
+            let display_value = match value_ref {
+                ValueRef::Null => "NULL".to_string(),
+                ValueRef::Integer(i) => i.to_string(),
+                ValueRef::Real(f) => f.to_string(),
+                ValueRef::Text(t) => String::from_utf8_lossy(t).to_string(),
+                ValueRef::Blob(b) => format!("[BLOB, len={}]", b.len()),
+            };
+            cells.push(display_value);
+        }
+        builder.push_record(cells);
+    }
+
+    let mut table = builder.build();
+    table.with(Style::modern());
+    table.modify(Columns::first(), Alignment::right());
+    println!("{table}");
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,16 @@
-use rusqlite::{params, Connection, Result};
-use rusqlite::types::{ValueRef};
+use rusqlite::{Connection, Result, params};
+use sql_trainer::execute_user_select;
 use std::io;
-use tabled::{
-    Tabled,
-    settings::{Style, Alignment, object::Columns},
-    builder::Builder
-};
-
-#[derive(Tabled)]
-#[derive(Debug)]
-struct Person {
-    id: i32,
-    name: String,
-    data: String,
-}
-
-fn execute_user_select(conn: &Connection, query: &str) -> Result<()> {
-    let mut stmt = conn.prepare(query)?;
-    let col_count = stmt.column_count();
-
-    let col_names: Vec<String> = stmt
-        .column_names()
-        .iter()
-        .map(|s| String::from(*s))
-        .collect::<Vec<_>>();
-
-    let mut rows = stmt.query([])?;
-
-    let mut builder = Builder::default();
-    builder.push_record(col_names.iter().map(|h| h.clone()));
-
-    
-    while let Some(row) = rows.next()? {
-        let mut cells = Vec::new();
-        for i in 0..col_count {
-            let value_ref = row.get_ref(i)?;
-            
-            let display_value = match value_ref {
-                ValueRef::Null => "NULL".to_string(),
-                ValueRef::Integer(i) => i.to_string(),
-                ValueRef::Real(f) => f.to_string(),
-                ValueRef::Text(t) => String::from_utf8_lossy(t).to_string(),
-                ValueRef::Blob(b) => format!("[BLOB, len={}]", b.len()),
-            };
-            cells.push(display_value);
-        }
-        builder.push_record(cells);
-    }
-
-    let mut table = builder.build();
-    table.with(Style::modern());
-    table.modify(Columns::first(), Alignment::right());
-    println!("{table}");
-
-    Ok(())
-}
 
 fn main() -> Result<()> {
     let db = std::env::args().nth(1).expect("No db given");
     println!("db: {:?}", db);
-    
+
     println!("SQL-QUERY:");
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read Input");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read Input");
     println!("SQL-QUERY: {input}");
 
     let connection = Connection::open_in_memory()?;
@@ -79,7 +27,7 @@ fn main() -> Result<()> {
     connection.execute(
         "INSERT INTO person (id, name, data) VALUES (?1, ?2, ?3)",
         params![0, "paul", "something"],
-    )?;    let mut data = connection.prepare("SELECT * FROM person")?;
+    )?;
 
     execute_user_select(&connection, "SELECT * FROM person;")?;
 
